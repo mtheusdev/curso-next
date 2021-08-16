ec455e09c86e7d67b4274e706dae733ebfe601dd
mtheusdev <matheus.santos@edu.udesc.br>
2021-08-16 13:58:02 -0300

    163-170 - finalizando 170

---
+++ src/components/template/Logo.tsx
@@ -0,0 +1,17 @@
+interface GenericProps {
+
+}
+
+const Generic = (props: GenericProps) => {
+  return (
+      <div className={'bg-white h-12 w-12 rounded-full flex flex-col items-center justify-center'}>
+        <div className={"h-3 w-3 rounded-full bg-red-600 mb-0.5"}></div>
+        <div className={"flex mt-0.5"}>
+          <div className="h-3 w-3 rounded-full bg-yellow-600 mr-0.5"></div>
+          <div className="h-3 w-3 rounded-full bg-green-600 ml-0.5"></div>
+        </div>
+      </div>
+  );
+};
+
+export default Generic;
--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,7 +1,20 @@
+import MenuItem from "./MenuItem";
+import Logo from './Logo'
+import { IconeAjustes, IconeCasa, IconeSino, IconeSair } from './../icons';
 const MenuLateral = () => {
   return (
-      <aside>
-        <h1>MenuLateral</h1>
+      <aside className="flex flex-col">
+        <div className={'h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800 flex flex-col items-center justify-center'}>
+          <Logo/>
+        </div>
+        <ul className="flex-grow">
+          <MenuItem url="/" texto="Início" icone={IconeCasa} />
+          <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
+          <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+        </ul>
+        <ul>
+          <MenuItem onClick={() => console.log("logout")} texto="Sair" icone={IconeSair} className={`text-red-600 hover:bg-red-400 hover:text-white`} />
+        </ul>
       </aside>
   );
 };
