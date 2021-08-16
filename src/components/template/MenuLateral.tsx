b5c19f944ed524e13900d026390b4e49f951c554
mtheusdev <matheus.santos@edu.udesc.br>
2021-08-16 11:25:24 -0300

    Iniciando projeto

---
+++ src/components/template/MenuLateral.tsx
@@ -0,0 +1,9 @@
+const MenuLateral = () => {
+  return (
+      <aside>
+        <h1>MenuLateral</h1>
+      </aside>
+  );
+};
+
+export default MenuLateral;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout"
+
+export default function Home() {
+  return (
+    <Layout titulo="Página inicial" subtitulo="Subtitulo ne mano">
+      <h3>Conteudo</h3>
+    </Layout>
+  )
+}
